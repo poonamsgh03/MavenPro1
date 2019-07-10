d7a3923ac7bf8e405d9a4a13fdab3137ba5bc2ae
poonamsingh <poonamsingh@192.168.0.5>
2019-07-10 19:02:27 +0530

    New maven project commited to git.

---
+++ https:/github.com/poonamsgh03/MavenPro1/maven.codebind/src/main/java/com/codebind/maven/codebind/App.java
@@ -0,0 +1,13 @@
+package com.codebind.maven.codebind;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
